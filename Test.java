9aae0d514fcb51c078efaed51781dcfe265703a3
zhassbala <besimbekovr@gmail.com>
2020-08-15 22:40:20 +0600

    most stable version for 15.08.2020, with mysql

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Test{
-	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt()
-		System.out.println(a + b);
-	}
-}
